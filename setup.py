345a5ff33b9144dec17d5a9984a9a71be5086d3d
Steve McGrath <smcgrath@tenable.com>
2021-07-19 16:46:08 -0500

    Upped version to 1.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ https://github.com/tenable/integrations-jira-cloud
 
 setup(
     name='tenable-jira-cloud',
-    version='1.1.23',
+    version='1.2.0',
     description='Tenable -> Jira Cloud Bridge',
     author='Tenable, Inc.',
     long_description=long_description,
